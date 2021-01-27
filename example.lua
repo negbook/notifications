d6197550703809a13db32767505f45faba6f346a
negbook <negbook@gmail.com>
2021-01-27 23:17:39 +0800

    Add files via upload

--- example.lua
+++ example.lua
@@ -3,15 +3,17 @@
 --[[
 Citizen.CreateThread(function()
     
-     ShowNotificatioMessagetext("Messagetext",'CHAR_MICHAEL','CHAR_MICHAEL',2,"NAME2","MESSAGE TEXT","(+#NEGBK",3,false,GetHudColor('HUD_COLOUR_WHITE'),1.0)
-     ShowNotificatioStats("Negbook","Scripting Skills",70,25,false,'CHAR_MICHAEL','CHAR_MICHAEL')
-     ShowNotificationTicker("TICKER",false,true)
-     ShowNotificationAward("YOU GOT AN AWARD!",'CHAR_MICHAEL', 'CHAR_MICHAEL', 333, GetHudColor('HUD_COLOUR_WHITE'), "GOLD THROPY")
-     ShowNotificationUnlock("UNLOCK",3,"UNLOCKSUB",false,GetHudColor('HUD_COLOUR_BLUE'))
-     ShowNotificatioCrewRankup("Negbook:","YOU DID A GOOD JOB!!!",'CHAR_MICHAEL','CHAR_MICHAEL',0)
+     ShowNotificatioMessagetext("TITLE","DESCRIPTION","(+#NEGBK","Content",'CHAR_MICHAEL','CHAR_MICHAEL',2,3,GetHudColor('HUD_COLOUR_WHITE'),1.0)
+     ShowNotificatioMessagetext("TITLE2","DESCRIPTION2","@*#NEGB","Content2",'CHAR_MICHAEL','CHAR_MICHAEL',2,3,GetHudColor('HUD_COLOUR_WHITE'),1.0)
+     ShowNotificatioMessagetext("TITLE3","DESCRIPTION3","#0xNEGB","Content3",'CHAR_MICHAEL','CHAR_MICHAEL',2,3,GetHudColor('HUD_COLOUR_WHITE'),1.0)
+     ShowNotificatioStats("Negbook","Scripting Skills",25,70,'CHAR_MICHAEL','CHAR_MICHAEL')
+     ShowNotificationTicker("TICKER")
+     ShowNotificationAward("GOLD THROPY","YOU GOT AN AWARD!",333, 'CHAR_MICHAEL', 'CHAR_MICHAEL', GetHudColor('HUD_COLOUR_WHITE'))
+     ShowNotificationUnlock("UNLOCK",3,"UNLOCKSUB",GetHudColor('HUD_COLOUR_BLUE'))
+     ShowNotificatioCrewRankup("Negbook:","YOU DID A GOOD JOB!!!",'CHAR_MICHAEL','CHAR_MICHAEL')
      ShowNotificationVersus('CHAR_FACEBOOK','CHAR_FACEBOOK',66,'CHAR_MICHAEL','CHAR_MICHAEL',33)
-     --ShowNotificatioReplay(1, "title",'subtitle',3)
-     --ShowNotificatioReplay(1, "title",'subtitle',"~INPUT_TALK~")
+     --ShowNotificatioReplay('subtitle',3)
+     --ShowNotificatioReplay('subtitle',"~INPUT_TALK~")
 end)
 
 
